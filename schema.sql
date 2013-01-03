8ec74fea17af511b8495a43c0fd7010ce0ae8470
Stefan Sabev <subevstefan@gmail.com>
2013-01-03 18:05:59 +0200

    excluding db files and adding a comment table. Will add more functionality later

--- schema.sql
+++ schema.sql
@@ -4,3 +4,13 @@ create table entries (
   title string not null,
   text string not null
 );
+
+drop table if exists comments;
+create table comments (
+  comment_id integer primary key autoincrement,
+  from_email string not null,
+  from_name string not null,
+  comment_text string not null,
+  p_id int,
+  foreign key (p_id) references entries(id)
+);
